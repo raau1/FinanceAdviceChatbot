8f46eda772743dd02eb1986008e0654ae8c8a6e5
Raul Blanco Vazquez <raulblancovazquezcontact@gmail.com>
2025-02-14 19:09:39 +0000

    Configured security for Railway deployment

--- src/main/java/com/financeadvicechatbot/config/SecurityConfig.java
+++ src/main/java/com/financeadvicechatbot/config/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    // Making the SecurityConfig object, with userDetails
     private final UserDetails userDetails;
 
     public SecurityConfig(UserDetails userDetails) {
@@ -21,34 +20,36 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Any Security configurations go here
         http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(request -> request.requestMatchers("/register", "/login",
-                        "/css/**", "/js/**", "/images/**", "/").permitAll()
-                // Any other request has to be authenticated
-                .anyRequest().authenticated());
+
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers("/", "/register", "/login", "/css/**", "/js/**", "/images/**").permitAll()
+                .anyRequest().authenticated()
+        );
+
         http.formLogin(form -> form
                 .loginPage("/login")
-                // Specifying we are using email not username
                 .usernameParameter("email")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/", true)
-                .permitAll());
+                .permitAll()
+        );
 
-        // Specify what happens when a user logs out
         http.logout(logout -> logout
-                .logoutSuccessUrl("/login")
-                .permitAll());
-        // Add custom userDetailsService for user authentication
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+        );
+
         http.userDetailsService(userDetails);
-        // Returning these configurations
+
         return http.build();
     }
 
-    // Creating a PasswordEncoder, to encode passwords on the database
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
